f98e8cc91c34352c8003b9a1371fecf8ec0e8876
Bahadır <omerbahadir28@gmail.com>
2026-06-02 22:44:06 +0300

    Update TABLOLARI  GÜNCELLEME.sql

--- TABLOLARI  GÜNCELLEME.sql
+++ TABLOLARI  GÜNCELLEME.sql
@@ -3,34 +3,33 @@ ALTER TABLE Uyeler ADD
     DogumTarihi DATE,
     Cinsiyet VARCHAR(10),
     AcilDurumTlf VARCHAR(15),
-    UyelikDurumu VARCHAR(10) DEFAULT 'Aktif';
-GO
+    UyelikDurumu VARCHAR(10) DEFAULT 'Aktif'
+
 
 -- 2. Egitmenler Tablosu Güncelleme
 ALTER TABLE Egitmenler ADD 
     Maas DECIMAL(10,2),
-    IseBaslamaTarihi DATE;
-GO
+    IseBaslamaTarihi DATE
+
 
 -- 3. UyelikPaketleri Tablosu Güncelleme
 ALTER TABLE UyelikPaketleri ADD 
-    GecerlilikSuresiAy INT;
-GO
+    GecerlilikSuresiAy INT
+
 
 -- 4. Dersler Tablosu Güncelleme
 ALTER TABLE Dersler ADD 
     DersGunu VARCHAR(15),
     ZorlukDerecesi VARCHAR(15),
-    DersSuresiDk INT;
-GO
+    DersSuresiDk INT
+
 
 -- 5. Odemeler Tablosu Güncelleme
 ALTER TABLE Odemeler ADD 
     IndirimOrani INT DEFAULT 0,
-    TaksitSayisi INT DEFAULT 1;
-GO
+    TaksitSayisi INT DEFAULT 1
+
 
 -- 6. KatilimKayitlari Tablosu Güncelleme
 ALTER TABLE KatilimKayitlari ADD 
-    KatilimDurumu VARCHAR(10);
-GO
+    KatilimDurumu VARCHAR(10)
