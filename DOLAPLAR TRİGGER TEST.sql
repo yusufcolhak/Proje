c38443923e6df986f9b54a12939b8cd521709fc8
Ahmet4326 <bayrakoglu43@gmail.com>
2026-06-03 15:20:41 +0300

    Add files via upload

---
+++ DOLAPLAR TRİGGER TEST.sql
@@ -0,0 +1,17 @@
+
+SELECT UyeID, Ad, Soyad, UyelikDurumu 
+FROM Uyeler 
+WHERE UyeID IN (16, 2); 
+
+
+INSERT INTO Dolaplar (DolapNo, UyeID, TahsisTarihi)
+VALUES (55, 16, GETDATE());
+
+
+
+INSERT INTO Dolaplar (DolapNo, UyeID, TahsisTarihi)
+VALUES (99, 2, GETDATE());
+
+
+
+
