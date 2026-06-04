2d12eff4e7cb3c99cee40aa63f834fe109552829
Yusuf <yusuf.colhak@gmail.com>
2026-06-04 14:52:04 +0300

    Update DOLAPLAR TRİGGER TEST.sql

--- DOLAPLAR TRİGGER TEST.sql
+++ DOLAPLAR TRİGGER TEST.sql
@@ -1,16 +1,16 @@
 
 SELECT UyeID, Ad, Soyad, UyelikDurumu 
 FROM Uyeler 
-WHERE UyeID IN (16, 2); 
+WHERE UyeID IN (16, 2)
 
 
 INSERT INTO Dolaplar (DolapNo, UyeID, TahsisTarihi)
-VALUES (55, 16, GETDATE());
+VALUES (55, 16, GETDATE())
 
 
 
 INSERT INTO Dolaplar (DolapNo, UyeID, TahsisTarihi)
-VALUES (99, 2, GETDATE());
+VALUES (99, 2, GETDATE())
 
 
 
