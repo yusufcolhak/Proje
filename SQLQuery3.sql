e181b5d5dc492bb71b6a3434631b547ccf50ae23
Yusuf <yusuf.colhak@gmail.com>
2026-06-04 14:25:38 +0300

    Delete SQLQuery3.sql

--- SQLQuery3.sql
+++
@@ -1,6 +0,0 @@
-/****** SSMS’ten SelectTopNRows komutu için betik  ******/
-SELECT TOP (1000) [PaketID]
-      ,[PaketAdi]
-      ,[Sure]
-      ,[Ucret]
-  FROM [SporSalonu].[dbo].[UyelikPaketleri]
