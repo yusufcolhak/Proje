d756e6aea73df4d5f56990fa7c562a1eb6a3b80b
Yusuf <yusuf.colhak@gmail.com>
2026-06-04 14:25:59 +0300

    Delete SQLQuery6.sql

--- SQLQuery6.sql
+++
@@ -1,7 +0,0 @@
-/****** SSMS’ten SelectTopNRows komutu için betik  ******/
-SELECT TOP (1000) [EgitmenID]
-      ,[Ad]
-      ,[Soyad]
-      ,[UzmanlikAlani]
-      ,[TelefonNumarasi]
-  FROM [SporSalonu].[dbo].[Egitmenler]
